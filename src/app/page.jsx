5e0ebc05edcc3ff64071664902d72fc58c7e8b06
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-31 00:26:10 -0300

    feat: add homepage main content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,18 +2,31 @@
 
 import React from "react";
 import styles from "./page.module.css";
-import { Layout } from 'antd'
+import { Layout, Flex, Typography, Space } from 'antd'
 import ThemeProvider from "../theme";
+
+const { Header, Content } = Layout
+const { Title, Text } = Typography
 
 export default function Home() {
 
   return (
     <ThemeProvider>
       <Layout>
-        <div className={styles.teste}>
-          Rock Doido
-        </div>
+        <Header>
+          <Title>QUIZ REFLEXIVO</Title>
+          <Title>COMO ESTÁ MEU TEMPO DE TELA?</Title>
+        </Header>
+        <Content>
+          <Space direction="vertical">
+            <Text>INSTRUÇÕES</Text>
+            <Text>Responda com sinceridade</Text>
+            <Text>Este quiz não tem certo ou errado, são 10 perguntas que servem para pensar sobre seus próprios hábitos com o celular, computador, TV ou outros aparelhos</Text>
+            <Text>No final, descubra o que suas respostas dizem sobre o seu uso das telas
+            </Text>
+          </Space>
+        </Content>
       </Layout>
-    </ThemeProvider>
+    </ThemeProvider >
   );
 }
