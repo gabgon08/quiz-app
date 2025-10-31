733eeec30ac459624bed7fda28421d4a9d432394
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-31 04:54:36 -0300

    feat: adjust alignment on home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,9 @@
 
 import React from "react";
 import styles from "./page.module.css";
-import { Layout, Typography, Space } from 'antd'
+import { Layout, Typography, Space, Button } from 'antd'
 import ThemeProvider from "../theme";
+import Link from "next/link";
 
 const { Header, Content } = Layout
 const { Title, Text } = Typography
@@ -12,20 +13,23 @@ export default function Home() {
 
   return (
     <ThemeProvider>
-      <Layout>
-        <Header>
-          <Title>QUIZ REFLEXIVO</Title>
-          <Title>COMO ESTÁ MEU TEMPO DE TELA?</Title>
-        </Header>
-        <Content>
-          <Space direction="vertical">
-            <Text>INSTRUÇÕES</Text>
-            <Text>Responda com sinceridade</Text>
+      <Layout className={styles.layout}>
+
+        <Content className={styles.content}>
+          <Space direction="vertical" align="center" className={styles.space}>
+            <Title level={1}>QUIZ REFLEXIVO</Title>
+            <Title level={1}>COMO ESTÁ MEU TEMPO DE TELA?</Title>
+            <Title level={2}>INSTRUÇÕES</Title>
+            <Text strong>‼️Responda com sinceridade‼️</Text>
             <Text>Este quiz não tem certo ou errado, são 10 perguntas que servem para pensar sobre seus próprios hábitos com o celular, computador, TV ou outros aparelhos</Text>
             <Text>No final, descubra o que suas respostas dizem sobre o seu uso das telas
             </Text>
+            <Button>
+              <Link href='/quiz'>Iniciar</Link>
+            </Button>
           </Space>
         </Content>
+
       </Layout>
     </ThemeProvider >
   );
